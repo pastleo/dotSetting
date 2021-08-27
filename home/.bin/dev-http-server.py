79e6a0ce5852f722188287578be86cf63c5574c3
PastLeo <chgu82837@gmail.com>
2021-08-27 11:25:21 +0800

    add dev-http-server.py

---
+++ home/.bin/dev-http-server.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+# https://gist.github.com/ccoenen/8038234
+# https://gist.github.com/dustingetz/5348582
+
+import http.server # python3
+# import SimpleHTTPServer # python2
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_my_headers()
+        http.server.SimpleHTTPRequestHandler.end_headers(self)
+
+    def send_my_headers(self):
+        self.send_header("Cache-Control", "no-cache, no-store, must-revalidate")
+        self.send_header("Pragma", "no-cache")
+        self.send_header("Expires", "0")
+
+
+if __name__ == '__main__':
+    http.server.test(HandlerClass=MyHTTPRequestHandler)
