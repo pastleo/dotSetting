eeb8368105aa8da060220da82ee7f0a22d1ecf08
PastLeo <chgu82837@gmail.com>
2023-02-13 03:34:52 +0800

    adjust everforest nvim colors

--- home/.config/nvim/lua/colors.lua
+++ home/.config/nvim/lua/colors.lua
@@ -4,11 +4,22 @@ end
 
 vim.opt.background = 'dark'
 
-vim.g.everforest_background = 'soft'
 vim.g.everforest_enable_italic = 1
 vim.g.everforest_better_performance = 1
 vim.g.everforest_transparent_background = 1
-vim.g.everforest_colors_override = { bg_visual = { '#4f5b58',  '240' } }
+
+vim.g.everforest_colors_override = {
+  fg = { '#abd4bb', 'NONE' },
+  aqua = { '#83c0c0', '108' },
+  blue = { '#8393c0', '109' },
+  grey0 = { '#a2aaa1',  '243' },
+  grey1 = { '#aeb7b0',  '245' },
+  grey2 = { '#bbc4bd',  '247' },
+  bg2 = { '#374542',  '247' },
+  bg3 = { '#809199',  '247' },
+  bg_visual = { '#4f5b58',  '240' },
+  bg_blue = { '#ffffff', '17' }
+}
 
 vim.cmd[[
   silent! colorscheme everforest
