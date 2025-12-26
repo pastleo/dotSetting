ea75570c499f9e43776282656a411d95362ddd36
PastLeo <chgu82837@gmail.com>
2025-12-26 15:33:44 +0800

    add opencode config

---
+++ install.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+targets=${@:-home}
+
+# cd to SCRIPT_DIR
+cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null
+echo "pwd: $(pwd)"
+
+echo "Will do:"
+echo "  stow -t ~ --no-folding $targets"
+echo ""
+echo "BTW, if there are existing files not stowed, please delete manually"
+
+read -p "(Press Y to continue) " -n 1 -r
+echo ""
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+  stow -t ~ --no-folding $targets
+  echo "Done."
+then
+  [[ "$0" = "$BASH_SOURCE" ]] && exit 1 || return 1 # handle exits from shell or function but don't exit interactive shell
+fi
+
