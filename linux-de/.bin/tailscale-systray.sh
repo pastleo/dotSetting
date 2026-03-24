c45d655df648fd77498614bf69234fe8f8215706
PastLeo <chgu82837@gmail.com>
2026-03-24 12:01:17 +0800

    add tailscale built-in systray script

---
+++ linux-de/.bin/tailscale-systray.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# for KDE: open system setting (KDE) -> System -> Autostart -> Add New -> Login script: `~/.bin/tailscale-systray.sh`
+# see more: https://tailscale.com/kb/1597/linux-systray
+
+tailscale systray
